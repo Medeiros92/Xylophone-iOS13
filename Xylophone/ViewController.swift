d7145dc2196a5752b58dff766e8139635ed164db
Medeiros92 <thalesbehringer@gmail.com>
2022-02-20 18:05:43 -0300

    Functions Update, Commit Update

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -19,9 +19,11 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         let letter = sender.currentTitle
+        
+        //Executa o som do botao clicado
         playSound(comand : letter!)
     }
-    
+    //Função que recebe o botao clicado
     func playSound(comand :String) {
         let url = Bundle.main.url(forResource: comand, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
